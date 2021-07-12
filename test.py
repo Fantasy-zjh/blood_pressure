99cb1cc97378867490460ee8336392ab044a759c
Fantasy-zjh <296780769@qq.com>
2021-07-12 16:36:49 +0800

    预测结果进行比较

--- test.py
+++ test.py
@@ -13,6 +13,8 @@ import matplotlib.pyplot as plt
 import math
 from scipy import signal
 from MIMICData import MIMICHelper
+from scipy.fftpack import fft, ifft
+import matplotlib.pyplot as plt
 
 
 # 计算两点之间的距离
@@ -21,15 +23,6 @@ def eucliDist(A, B):
 
 
 if __name__ == "__main__":
-    # data = [[1, 2, 3, 4], [2, 3, 4, 5], [3, 4, 5, 6], [4, 5, 6, 7]]
-    # data = [signal.resample(d, 5).tolist() for d in data]
-    # print(data)
-    # print(type(data))
-    d = [[1, 2, 3, 4], [2, 3, 4, 5], [3, 4, 5, 6], [4, 5, 6, 7]]
-    d = array(d)
-    out = nonzero(d[:, 0] > 2)[0].tolist()
-    print(out)
-    print(type(out))
-    data = [1, 2, 3, 4]
-    mic = MIMICHelper()
-    mic.writeToFile2(data, mic.MIMIC_ONE_1000_PATH + "center.cluster")
+    a = [[1, 10], [3, 20]]
+    a_r = np.array(a)
+    print(a_r.mean(axis=0))
